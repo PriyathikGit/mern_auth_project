b83c38aab7c5139d6716fc89c73174a78bac4c63
PriyathikGit <techhack237@gmail.com>
2025-02-08 19:09:46 +0530

    add dotenv and mongoose for environment configuration and database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
-import express from "express"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const DB_NAME = 'auth_backend';
+mongoose
+  .connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  .then(() => {
+    console.log('connected to backend');
+  })
+  .catch((err) => console.log(err));
 
-const app = express()
+const app = express();
 
-app.listen(3000,()=>{
-    console.log("server is listening on port 3000")
-})
+app.listen(3000, () => {
+  console.log('server is listening on port 3000');
+});
